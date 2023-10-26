5e977edc7f11bc084873ae428c15d9f7a8433428
ryryry-3302 <rjleong2002@gmail.com>
2023-10-26 11:36:40 +0800

    fixed stuff

---
+++ working.cpp
@@ -0,0 +1,239 @@
+#include <MeMCore.h>
+MeBuzzer buzzer;
+
+#define TURNING_TIME_MS 344.5 // The time duration (ms) for turning
+
+#define TIMEOUT 1600 // Max microseconds to wait; choose according to max distance of wall
+#define SPEED_OF_SOUND 340 // Update according to your own experiment
+#define ULTRASONIC 10
+
+#define BIT_A_ORANGE A2
+#define BIT_B_YELLOW A3
+#define LDR A0
+#define ir_receiver A1
+
+int  ambient  = 680;
+
+
+float colourArray[] = {0,0,0};
+float whiteArray[] = {0,0,0};
+float blackArray[] = {0,0,0};
+float greyDiff[] = {0,0,0};
+
+int red = 0;
+int green = 0;
+int blue = 0;
+int right_distance = 1;
+int left_distance = 0;
+
+MeDCMotor leftMotor(M1); // assigning leftMotor to port M1
+MeDCMotor rightMotor(M2); // assigning RightMotor to port M2
+uint8_t motorSpeed = 255;
+
+double gen_ultrasonic() {
+    pinMode(ULTRASONIC, OUTPUT);
+    digitalWrite(ULTRASONIC, LOW);
+    delayMicroseconds(2);
+    digitalWrite(ULTRASONIC, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(ULTRASONIC, LOW);
+    pinMode(ULTRASONIC, INPUT);
+    long duration = pulseIn(ULTRASONIC, HIGH, TIMEOUT);
+    if (duration > 0) {
+    double distance = duration / 2.0 / 1000000 * SPEED_OF_SOUND * 100 - 3.40;
+    Serial.print("distance(cm) = ");
+   Serial.println(distance);
+    return distance;
+    }
+    else {
+   //  Serial.println("out of range");
+    return 0;
+    }
+}
+
+void celebrate() {
+    // Code for playing celebratory tune
+    int e = 329;
+    int f = 349;
+    int g = 370;
+    int d = 277;
+    int c = 247;
+    buzzer.tone(e, 600);
+    buzzer.tone(e, 600);
+    buzzer.tone(f, 600);
+    buzzer.tone(g, 600);
+    buzzer.tone(g, 600);
+    buzzer.tone(f, 600);
+    buzzer.tone(e, 600);
+    buzzer.tone(d, 600);
+    buzzer.tone(c, 600);
+    buzzer.tone(c, 600);
+    buzzer.tone(d, 600);
+    buzzer.tone(e, 600);
+    buzzer.tone(e, 600);
+    buzzer.tone(d, 600);
+    buzzer.tone(d, 600);
+    buzzer.noTone();
+    delay(500);
+    buzzer.tone(e, 600);
+    buzzer.tone(e, 600);
+    buzzer.tone(f, 600);
+    buzzer.tone(g, 600);
+    buzzer.tone(g, 600);
+    buzzer.tone(f, 600);
+    buzzer.tone(e, 600);
+    buzzer.tone(d, 600);
+    buzzer.tone(c, 600);
+    buzzer.tone(c, 600);
+    buzzer.tone(d, 600);
+    buzzer.tone(e, 600);
+    buzzer.tone(d, 600);
+    buzzer.tone(c, 600);
+    buzzer.tone(c, 600);
+}
+void stopMotor() {
+    // Code for stopping motor
+    leftMotor.stop(); // Stop left motor
+    rightMotor.stop();
+}
+void moveForward() {
+    // Code for moving forward for some short interval
+    leftMotor.run(motorSpeed); // Positive: wheel turns clockwise
+    rightMotor.run(-motorSpeed);
+}
+void turnRight() {
+    // Code for turning right 90 deg
+    leftMotor.run(-motorSpeed); // Positive: wheel turns clockwise
+    rightMotor.run(-motorSpeed); // Positive: wheel turns clockwise
+    delay(TURNING_TIME_MS); // Keep turning left for this time duration
+    leftMotor.stop(); // Stop left motor
+    rightMotor.stop();
+}
+void turnLeft() {
+    // Code for turning left 90 deg
+    leftMotor.run(motorSpeed); // Positive: wheel turns clockwise
+    rightMotor.run(motorSpeed); // Positive: wheel turns clockwise
+    delay(TURNING_TIME_MS); // Keep turning left for this time duration
+    leftMotor.stop(); // Stop left motor
+    rightMotor.stop(); // Stop right motor
+    }
+void uTurn() {
+    // Code for u-turn
+    turnLeft();
+    delay(1500);
+    turnLeft();
+    delay(1500);
+    leftMotor.stop(); // Stop left motor
+    rightMotor.stop(); 
+}
+void doubleLeftTurn() {
+    // Code for double left turn
+    }
+void doubleRightTurn() {
+    // Code for double right turn
+    }
+void nudgeLeft() {
+    // Code for nudging slightly to the left for some short interval 
+    leftMotor.run(motorSpeed); // Positive: wheel turns clockwise
+    rightMotor.run(motorSpeed); // Positive: wheel turns clockwise
+    delay(40); // Keep turning left for this time duration
+    leftMotor.stop(); // Stop left motor
+    rightMotor.stop();
+}
+void nudgeRight() {
+    // Code for nudging slightly to the right for some short interval 
+    leftMotor.run(-motorSpeed); // Positive: wheel turns clockwise
+    rightMotor.run(-motorSpeed); // Positive: wheel turns clockwise
+    delay(40); // Keep turning left for this time duration
+    leftMotor.stop(); // Stop left motor
+    rightMotor.stop();
+}
+void shineIR() {
+    // Code for turning on the IR emitter only
+    digitalWrite(BIT_A_ORANGE, HIGH);
+    digitalWrite(BIT_B_YELLOW, HIGH);
+}
+void shineRed() {
+    // Code for turning on the red LED only
+    digitalWrite(BIT_A_ORANGE, LOW); 
+    digitalWrite(BIT_B_YELLOW, LOW); 
+}
+void shineGreen() {
+    // Code for turning on the green LED only
+    digitalWrite(BIT_A_ORANGE, LOW); 
+    digitalWrite(BIT_B_YELLOW, HIGH);
+}
+void shineBlue() {
+    // Code for turning on the blue LED only
+    digitalWrite(BIT_A_ORANGE, HIGH); 
+    digitalWrite(BIT_B_YELLOW, LOW);
+}
+
+int detectColour()
+{
+// Shine Red, read LDR after some delay
+// Shine Green, read LDR after some delay
+// Shine Blue, read LDR after some delay
+// Run algorithm for colour decoding
+}
+
+int calibrate_ir(){
+  shineRed();
+  delay(1000);
+  int temp = 0;
+  for (int i=0; i<10; i++){
+    temp += analogRead(ir_receiver);
+  }
+  ambient = temp / 10;
+  Serial.print("Ambient");
+  Serial.println(ambient);
+}
+
+
+
+void setup()
+{
+// Configure pinMode for A0, A1, A2, A3
+Serial.begin(9600); // to initialize the serial monitor
+pinMode(BIT_A_ORANGE, OUTPUT);
+pinMode(BIT_B_YELLOW, OUTPUT);
+pinMode(LDR, INPUT);
+calibrate_ir();
+}
+void loop()
+{
+// Read ultrasonic sensing distance (choose an appropriate timeout)
+moveForward();
+right_distance = gen_ultrasonic();
+shineIR();
+left_distance = analogRead(ir_receiver) - ambient;
+    if (right_distance != 0)
+    {
+        if (right_distance <4.0)
+        {
+            nudgeLeft();
+
+            moveForward();
+        }
+        else if (right_distance >=4.7)
+        {
+            nudgeRight();
+
+            moveForward();
+        }
+        else
+        {
+            moveForward();
+        }    
+    }
+    delay(20);
+    
+    
+
+
+// Read IR sensing distance (turn off IR, read IR detector, turn on IR, read IR detector, estimate distance)
+// if within black line, stop motor, detect colour, and take corresponding action
+// else if too near to left wall, nudge right
+// else if too near to right wall, nudge left
+// else move forward
+}
